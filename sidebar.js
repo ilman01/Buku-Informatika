7f6e7b7935d5ef694d9deae64dd015de71cd118e
ilman01 <ilmanzidni2006@gmail.com>
2025-02-05 16:36:05 +0700

    sidebar fix for chrome ios

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,7 @@ const sidebar = `
 <button class="btn btn-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation" style="position: fixed; left: 10px; z-index: 1050;">
     ☰
 </button>
-<div id="sidebar" class="sidebar text-white collapse" style="position: fixed; overflow: auto; max-height: 93vh; width: 250px; background-color: #232323; padding: 20px; transition: width 0.3s ease-in-out, height 0.3s ease-in-out;">
+<div id="sidebar" class="sidebar text-white collapse" style="position: fixed; overflow: auto; max-height: 93vh; width: 250px; background-color: #232323; padding: 20px; padding-bottom: 120px; transition: width 0.3s ease-in-out, height 0.3s ease-in-out;">
     <h4 style="padding-top: 40px;"><a class="text-white text-decoration-none" href="/buku/bab1.html">Hardware dan Software</a></h4>
     <ul class="nav flex-column">
         <li class="nav-item"><a class="nav-link text-white-50 bar" href="/buku/bab1.html#1-pendahuluan">Pendahuluan</a></li>
